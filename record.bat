5ac585faf997d007b4c491f80cb308ed22fdfad8
kevin <kevinx1698@gmail.com>
2024-09-23 00:34:44 -0400

    scripting update

--- record.bat
+++ record.bat
@@ -1,6 +1,14 @@
 @echo off
+REM Get the current date
+for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value ^| find "="')
 
-start cmd /k "muselsl stream"
+REM Format the date
+set "current_date=%datetime:~4,2%_%datetime:~6,2%"
+
+start cmd /k "muselsl stream -p -c -g"
+
+timeout /t 20 /nobreak
+
 start cmd /k "muselsl record -d 30000 -dj True -t EEG"
 start cmd /k "muselsl record -d 30000 -dj True -t PPG"
 start cmd /k "muselsl record -d 30000 -dj True -t ACC"
