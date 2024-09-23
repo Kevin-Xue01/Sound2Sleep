afe1dc2ccb622786cd0ff57fa5ea050f87b6ad77
Kevin <kevinx1698@gmail.com>
2024-09-23 00:19:24 -0400

    scripting done

--- record.bat
+++ record.bat
@@ -1,3 +1,7 @@
 @echo off
 
 start cmd /k "muselsl stream"
+start cmd /k "muselsl record -d 30000 -dj True -t EEG"
+start cmd /k "muselsl record -d 30000 -dj True -t PPG"
+start cmd /k "muselsl record -d 30000 -dj True -t ACC"
+start cmd /k "muselsl record -d 30000 -dj True -t GYRO"
