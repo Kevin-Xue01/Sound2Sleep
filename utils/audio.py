de6df3c344cec74c421d44cd05fe280326f4b73f
Kevin <kevinx1698@gmail.com>
2025-03-19 03:51:22 -0400

    new audio

--- utils/audio.py
+++ utils/audio.py
@@ -1,17 +1,16 @@
-import asyncio
 import math
-import sys
+import threading
 
 import colorednoise
 import numpy as np
 import simpleaudio as sa
-from PyQt5.QtCore import QRunnable, QThreadPool, QTimer
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import QRunnable
 
 from .config import AudioConfig
 
 
 class Audio(QRunnable):
+    _lock = threading.Lock()
     def __init__(self, config: AudioConfig):
         super().__init__()
         fs = 44100
@@ -32,17 +31,12 @@ class Audio(QRunnable):
         noisedata -= np.mean(noisedata)
         noisedata /= np.max(np.abs(noisedata))
         noisedata *= 32766
-        noisedata *= config.volume
         noisedata = noisedata.astype(np.int16)
 
         self.sound = sa.WaveObject(noisedata, 1, 2, fs)
 
-    async def play(self):
-        """Non-blocking audio playback using asyncio."""
-        loop = asyncio.get_running_loop()
-        await loop.run_in_executor(None, self._blocking_play)
-
-    def _blocking_play(self):
-        """Plays the sound in a blocking way (but wrapped in an executor)."""
-        handle = self.sound.play()
-        handle.wait_done()
+    def run(self):
+        # handle = self.sound.play()
+        # handle.wait_done()
+        with self._lock:  # Prevent overlapping playbacks
+            self.sound.play()  # Non-blocking playback
