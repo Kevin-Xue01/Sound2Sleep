931ac8e8161958fe7029dbafaa57fd1c9491677e
Vicki <Vickili852@gmail.com>
2025-03-27 23:04:44 -0400

    config and algo updates

--- utils/config.py
+++ utils/config.py
@@ -3,6 +3,7 @@ import os
 import random
 import string
 from datetime import datetime
+import numpy as np
 
 from dotenv import load_dotenv
 from pydantic import BaseModel, Field, PrivateAttr
@@ -20,14 +21,14 @@ class CLASAlgoConfig(BaseModel):
 
     hl_ratio_buffer_len: int = 5
     hl_ratio_wavelet_freqs: list = [10, 20, 30]
-    hl_ratio_latest_threshold: float = 0.15
+    hl_ratio_latest_threshold: float = 0.10
     hl_ratio_buffer_threshold: float = 0.1
 
     amp_buffer_len: int = 10
     amp_threshold: float = 75.0
     amp_limit: float = 300.0
 
-    target_phase: float = 0.0 # radians
+    target_phase: float = 6 * np.pi / 4 # radians
     backoff_time: float = 7.0
 
     quadrature_thresh: float = 0.2
@@ -40,7 +41,7 @@ class CLASAlgoConfig(BaseModel):
     stim2_prediction_limit_sec: float = 0.15
 
 class AudioConfig(BaseModel):
-    ramp_s: float = 0.01
+    ramp_s: float = 0.005
     total_s: float = 0.05
     volume: float = 0.1
 
